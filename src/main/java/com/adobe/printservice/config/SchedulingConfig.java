66ee7ad6a203519edf28ecf0c1a5ed4aac6d7e1b
Sergi Llobet <sergi.llobet@estudiants.urv.cat>
2026-09-07 18:06:54 +0200

    feat: enable Spring scheduling support

---
+++ src/main/java/com/adobe/printservice/config/SchedulingConfig.java
@@ -0,0 +1,9 @@
+package com.adobe.printservice.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+@Configuration
+@EnableScheduling
+public class SchedulingConfig {
+}
